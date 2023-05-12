ecfd2b1871b0328f6fc0bc4c9f0f6d6c72c0fffd
codewurld <sholaqdev@gmail.com>
2023-05-12 13:28:03 +0100

    implements resolvers

---
+++ server/src/resolvers.js
@@ -0,0 +1,23 @@
+// populates data for each field in schema
+const resolvers = {
+    Query: {
+        // returns an array of Tracks 
+        // to be used to populate homepage 
+        // grid of our client
+        // (parent, args, contextValue, info)
+        // deconstruct dataSources from contextVal
+        tracksForHomePage: (_, __, { dataSources }) => {
+            // interacts with our data source
+            return dataSources.trackAPI.getTracksForHomePage()
+        }
+    },
+    // indicates this is for the Track type in the schema
+    Track: {
+        // destructure authorId from author's parent (Track)
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
+    }
+};
+
+module.exports = resolvers;
